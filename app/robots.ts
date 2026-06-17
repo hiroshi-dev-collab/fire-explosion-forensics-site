159918cff38dfced95bd539e9470b9f019ad2f0d
hiroshi <hiroshi.seniordev@outlook.com>
2026-06-17 23:06:46 +0200

    chore: disallow WordPress paths in robots.txt to cut legacy crawl noise

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,18 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: [
+        "/wp-admin/",
+        "/wp-content/",
+        "/wp-includes/",
+        "/wp-login.php",
+        "/xmlrpc.php",
+        "/feed/",
+        "/category/",
+        "/author/",
+        "/?p=",
+        "/?s=",
+      ],
     },
     sitemap: "https://lauden.com.br/sitemap.xml",
   };
